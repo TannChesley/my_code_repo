fafe93ea59424ed767fa8dc1a9d3a746c1500b90
tannchesley <tann.chesley@gmail.com>
2022-07-08 14:29:02 -0400

    com

---
+++ loop.py
@@ -0,0 +1,14 @@
+#sample code for loops
+
+print("sample of counting down")
+i = 3
+while i != 0:
+    print("message here")
+    i = i-1
+    
+print("sample of counting up")
+
+# i = 0
+# while i < 3:
+    # print("message here")
+    # i =+ 1
